dda7c1da8576968e0966d505806319dc258c9009
dhruv <dhruvpal.2702@gmail.com>
2024-07-16 06:03:53 +0530

    secondcooooommiiiit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      colors: {
+        skin: {
+          base1: "var(--primary-color)",
+          base2: "var(--secondary-color)",
+          base3: "var(--btn-glass)",
+          base4: "var(--btn-border)",
+        },
+      },
       zIndex: {
         Infinity: "999999999999",
         bottomest: "-1",
         t2: "2",
+      },
+      height: {
+        v20: "20vh",
       },
     },
   },
